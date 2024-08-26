ad4a0c06505fb1ca63267e26c84bf6095dbcc84d
eugeniarioboacosta <eriobovilas@gmail.com>
2024-08-26 21:45:27 +0200

    Fix test

--- src/test/specs/test.e2e.js
+++ src/test/specs/test.e2e.js
@@ -1,6 +1,7 @@
 const { expect, browser, $ } = require('@wdio/globals')
 
 describe('My Login application', () => {
+
     it('should login with valid credentials', async () => {
         await browser.url(`https://the-internet.herokuapp.com/login`)
 
